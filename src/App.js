1fd36f719a95e5a2756c95985cdd7364ffcd7ac9
Alejandro Mejia <alejomejiacuartas@outlook.com>
2020-12-05 10:05:22 -0500

    Add Dashboard view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+// Importing from packages
 import { ThemeProvider } from 'styled-components';
+
+// Importing from config folder
 import light from './config/themes/light';
 import dark from './config/themes/dark';
 import GlobalCSS from './config/global-css';
 
-import Container from './components/Container';
+// Importing views
+import Dashboard from './views/Dashboard';
 
 function App() {
   return (
     <ThemeProvider theme={light}>
       <GlobalCSS />
-      <Container>Test</Container>
+      <Dashboard />
     </ThemeProvider>
   );
 }

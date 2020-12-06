2d63f1c44a8e87961a5693df00b765ffc09e3b37
Alejandro Mejia <alejomejiacuartas@outlook.com>
2020-12-06 11:20:19 -0500

    Add FadeInUp transition to Modal open and close

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,6 +2,9 @@
 import { useStoreActions } from 'easy-peasy';
 import PropTypes from 'prop-types';
 
+// Importing transitions
+import FadeInUp from '../Transitions/FadeInUp';
+
 // Importing from services
 import { renderSocialIcon, renderTypeIcon } from '../../services/';
 
@@ -25,73 +28,77 @@ const Modal = ({ open = false }) => {
     <>
       {open && (
         <Root className="Modal">
-          <div className="Modal__container">
-            <div className="Modal__card">
-              <div className="Modal__card-close">
-                <button
-                  className="Modal__card-close-button"
-                  onClick={handleClick}
-                >
-                  <img
-                    className="Modal__card-close-icon"
-                    src={iconClose}
-                    alt="close icon"
-                  />
-                </button>
-              </div>
-              <div className="Modal__card-header">
-                <h3 className="Modal__title">Facebook followers</h3>
-                <div className="Modal__social">
-                  <img
-                    className="Modal__social-icon"
-                    src={renderSocialIcon('facebook')}
-                    alt="Facebook icon"
-                  />
-                  <span className="Modal__social-username">@nathanf</span>
+          <FadeInUp>
+            <div className="Modal__container">
+              <div className="Modal__card">
+                <div className="Modal__card-close">
+                  <button
+                    className="Modal__card-close-button"
+                    onClick={handleClick}
+                  >
+                    <img
+                      className="Modal__card-close-icon"
+                      src={iconClose}
+                      alt="close icon"
+                    />
+                  </button>
                 </div>
-                <div className="Modal__data">
-                  <div className="Modal__data-item">
-                    <div className="Modal__data-quantity">
-                      <span className="Modal__data-quantity-number">1987</span>
-                    </div>
-                    <span className="Modal__data-text">Total followers</span>
+                <div className="Modal__card-header">
+                  <h3 className="Modal__title">Facebook followers</h3>
+                  <div className="Modal__social">
+                    <img
+                      className="Modal__social-icon"
+                      src={renderSocialIcon('facebook')}
+                      alt="Facebook icon"
+                    />
+                    <span className="Modal__social-username">@nathanf</span>
                   </div>
-                  <div className="Modal__data-item">
-                    <div className="Modal__data-quantity Modal__data-quantity--up">
-                      <img
-                        className="Modal__data-quantity-icon"
-                        src={renderTypeIcon('up')}
-                        alt="up icon"
-                      />
-                      <span className="Modal__data-quantity-number">81</span>
+                  <div className="Modal__data">
+                    <div className="Modal__data-item">
+                      <div className="Modal__data-quantity">
+                        <span className="Modal__data-quantity-number">
+                          1987
+                        </span>
+                      </div>
+                      <span className="Modal__data-text">Total followers</span>
                     </div>
-                    <span className="Modal__data-text">
-                      New followers in the past 10 days
-                    </span>
-                  </div>
-                  <div className="Modal__data-item">
-                    <div className="Modal__data-quantity Modal__data-quantity--up">
-                      <img
-                        className="Modal__data-quantity-icon"
-                        src={renderTypeIcon('up')}
-                        alt="up icon"
-                      />
-                      <span className="Modal__data-quantity-number">12</span>
+                    <div className="Modal__data-item">
+                      <div className="Modal__data-quantity Modal__data-quantity--up">
+                        <img
+                          className="Modal__data-quantity-icon"
+                          src={renderTypeIcon('up')}
+                          alt="up icon"
+                        />
+                        <span className="Modal__data-quantity-number">81</span>
+                      </div>
+                      <span className="Modal__data-text">
+                        New followers in the past 10 days
+                      </span>
                     </div>
-                    <span className="Modal__data-text">
-                      New followers TODAY
-                    </span>
+                    <div className="Modal__data-item">
+                      <div className="Modal__data-quantity Modal__data-quantity--up">
+                        <img
+                          className="Modal__data-quantity-icon"
+                          src={renderTypeIcon('up')}
+                          alt="up icon"
+                        />
+                        <span className="Modal__data-quantity-number">12</span>
+                      </div>
+                      <span className="Modal__data-text">
+                        New followers TODAY
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="Modal__card-body">
-                <div className="Modal__date">
-                  <h4 className="Modal__date-range">May 4 - May 13</h4>
+                <div className="Modal__card-body">
+                  <div className="Modal__date">
+                    <h4 className="Modal__date-range">May 4 - May 13</h4>
+                  </div>
+                  <div className="Modal__chart">CHART HERE</div>
                 </div>
-                <div className="Modal__chart">CHART HERE</div>
               </div>
             </div>
-          </div>
+          </FadeInUp>
         </Root>
       )}
     </>
--- src/components/Transitions/FadeInUp/styles.js
+++ src/components/Transitions/FadeInUp/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Root = styled.div`
   flex: 1 0 20%;
+  height: 100%;
 
   @media (max-width: ${(props) => props.theme.breakpoints.m}) {
     flex: 1 0 40%;

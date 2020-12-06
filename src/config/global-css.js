b6eeb87fa26f88d814e2dfc6dad3f94540bfe0b1
Alejandro Mejia <alejomejiacuartas@outlook.com>
2020-12-06 08:49:19 -0500

    Update global styles to reset buttons and add new close svg icon to assets

--- src/config/global-css.js
+++ src/config/global-css.js
@@ -22,8 +22,15 @@ const GlobalCSS = createGlobalStyle`
     position: relative;
     font-family: 'Inter', 'Verdana', sans-serif;
     font-size: 16px;
+
+    // Changing with the theme
     background-color: ${(props) => props.theme.colors.background};
     transition: ${(props) => props.theme.transitions('background-color')};
+  }
+
+  button{
+    border: 0;
+    background-color: inherit;
   }
 `;
 

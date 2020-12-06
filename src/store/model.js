243faa04727b2b5d456e4dc6ca9e8e0ba3493f77
Alejandro Mejia <alejomejiacuartas@outlook.com>
2020-12-05 22:50:55 -0500

    Add auto detect pc is dark mode to enable as default

--- src/store/model.js
+++ src/store/model.js
@@ -1,9 +1,13 @@
 // Importing from packages
 import { action } from 'easy-peasy';
 
+// Detect if pc is using dark mode to enable as default
+const detectDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
+  .matches;
+
 const model = {
   // State
-  isDarkMode: false,
+  isDarkMode: detectDarkMode,
   // Actions
   handleTheme: action((state, payload) => {
     state.isDarkMode = payload;

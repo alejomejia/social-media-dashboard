1d30df586eac9d66d78e72796d71d3bc1e7ed583
Alejandro Mejia <alejomejiacuartas@outlook.com>
2020-12-08 08:20:18 -0500

    Remove unused css attr on FadeInUp component

--- src/components/Transitions/FadeInUp/styles.js
+++ src/components/Transitions/FadeInUp/styles.js
@@ -1,14 +1,5 @@
 import styled from 'styled-components';
 
 export const Root = styled.div`
-  flex: 1 0 20%;
   height: 100%;
-
-  @media (max-width: ${(props) => props.theme.breakpoints.m}) {
-    flex: 1 0 40%;
-  }
-
-  @media (max-width: ${(props) => props.theme.breakpoints.xs}) {
-    flex: 1 0 50%;
-  }
 `;
